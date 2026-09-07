4f94614c8b58083830e39c6438681277573b2651
loon4k1356-stack <loon4k1356@gmail.com>
2026-09-07 21:37:00 +0300

    Create ClientPlayNetworkHandlerMixin.java

---
+++ src/main/java/com/basefinder/mixin/ClientPlayNetworkHandlerMixin.java
@@ -0,0 +1,9 @@
+package com.basefinder.mixin;
+
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public abstract class ClientPlayNetworkHandlerMixin {
+    // Empty mixin - placeholder for future packet interception
+}
