758816582ffe82d9a53114a71f2f434d9708224d
loon4k1356-stack <loon4k1356@gmail.com>
2026-09-08 16:49:18 +0300

    Update ModuleManager.java

--- src/main/java/com/basefinder/module/ModuleManager.java
+++ src/main/java/com/basefinder/module/ModuleManager.java
@@ -1,9 +1,32 @@
-public List<Module> getModulesByCategory(Module.Category category) {
-    List<Module> result = new ArrayList<>();
-    for (Module mod : modules) { // Предполагаем, что список называется modules
-        if (mod.getCategory() == category) {
-            result.add(mod);
+package com.basefinder.module;
+
+import com.basefinder.module.modules.BaseFinderModule;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModuleManager {
+
+    public List<Module> modules = new ArrayList<>();
+    public BaseFinderModule baseFinder;
+
+    public void registerModule(Module module) {
+        modules.add(module);
+        if (module instanceof BaseFinderModule) {
+            this.baseFinder = (BaseFinderModule) module;
         }
     }
-    return result;
+
+    public List<Module> getModules() {
+        return modules;
+    }
+
+    public List<Module> getModulesByCategory(Module.Category category) {
+        List<Module> result = new ArrayList<>();
+        for (Module module : modules) {
+            if (module.getCategory() == category) {
+                result.add(module);
+            }
+        }
+        return result;
+    }
 }
