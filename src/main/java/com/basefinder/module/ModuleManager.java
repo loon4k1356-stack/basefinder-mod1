4ee6a5e7beb811654c7a779e2b1ea67261bdd939
loon4k1356-stack <loon4k1356@gmail.com>
2026-09-08 16:47:27 +0300

    Update ModuleManager.java

--- src/main/java/com/basefinder/module/ModuleManager.java
+++ src/main/java/com/basefinder/module/ModuleManager.java
@@ -1,37 +1,9 @@
-package com.basefinder.module;
-
-import com.basefinder.module.modules.BaseFinderModule;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class ModuleManager {
-    private List<Module> modules;
-    public BaseFinderModule baseFinder;
-    
-    public ModuleManager() {
-        modules = new ArrayList<>();
-        baseFinder = new BaseFinderModule();
-        modules.add(baseFinder);
-    }
-    
-    public void onTick() {
-        for (Module module : modules) {
-            if (module.isEnabled()) module.onTick();
+public List<Module> getModulesByCategory(Module.Category category) {
+    List<Module> result = new ArrayList<>();
+    for (Module mod : modules) { // Предполагаем, что список называется modules
+        if (mod.getCategory() == category) {
+            result.add(mod);
         }
     }
-    
-    public List<Module> getModules() { return modules; }
-    
-    public List<Module> getModulesByCategory(Module.Category category) {
-        return modules.stream().filter(m -> m.getCategory() == category).collect(Collectors.toList());
-    }
-    
-    public Module getModuleByName(String name) {
-        return modules.stream().filter(m -> m.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
-    }
-    
-    public List<Module> getEnabledModules() {
-        return modules.stream().filter(Module::isEnabled).collect(Collectors.toList());
-    }
+    return result;
 }
