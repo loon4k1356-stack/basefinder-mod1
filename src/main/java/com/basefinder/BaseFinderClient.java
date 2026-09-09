d96a788a1f9a1e2afb13d5c35cb7d8d967e6b53f
loon4k1356-stack <loon4k1356@gmail.com>
2026-09-09 21:44:49 +0300

    Update BaseFinderClient.java

--- src/main/java/com/basefinder/BaseFinderClient.java
+++ src/main/java/com/basefinder/BaseFinderClient.java
@@ -5,7 +5,7 @@ import com.basefinder.gui.HudRenderer;
 import com.basefinder.gui.TargetHUD;
 import com.basefinder.module.ModuleManager;
 import com.basefinder.module.modules.BaseFinderModule;
-import com.basefinder.module.modules.KillAura; // Импорт новой ауры
+import com.basefinder.module.modules.KillAura;
 import com.basefinder.scanner.BlockScanner;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -32,7 +32,7 @@ public class BaseFinderClient implements ClientModInitializer {
     public static BaseFinderModule baseFinderModule;
     public static BlockScanner scanner;
     public static HudRenderer hudRenderer;
-    public static TargetHUD targetHUD; // Новый HUD
+    public static TargetHUD targetHUD;
 
     private static KeyBinding openGuiKey;
     private static KeyBinding toggleScannerKey;
@@ -45,13 +45,12 @@ public class BaseFinderClient implements ClientModInitializer {
         baseFinderModule = new BaseFinderModule();
         moduleManager.registerModule(baseFinderModule);
         
-        // Регистрация KillAura
         KillAura killAura = new KillAura();
         moduleManager.registerModule(killAura);
 
         scanner = new BlockScanner();
         hudRenderer = new HudRenderer();
-        targetHUD = new TargetHUD(); // Инициализация TargetHUD
+        targetHUD = new TargetHUD();
 
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.freezdlc.gui", GLFW.GLFW_KEY_RIGHT_SHIFT, "category.freezdlc"));
         toggleScannerKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.freezdlc.toggle", GLFW.GLFW_KEY_H, "category.freezdlc"));
@@ -65,16 +64,11 @@ public class BaseFinderClient implements ClientModInitializer {
             }
         });
 
-        // Рендер основного HUD
         HudRenderCallback.EVENT.register((drawContext, tickCounter) -> {
             if (hudRenderer != null) hudRenderer.render(drawContext, tickCounter);
-            if (targetHUD != null) targetHUD.render(drawContext, tickCounter); // Рендер TargetHUD
+            if (targetHUD != null) targetHUD.render(drawContext, tickCounter);
         });
 
-        // Перехват кликов мыши для перетаскивания TargetHUD
-        // Примечание: Для полноценной работы нужна регистрация события mouseClicked через Mixin или Fabric Event
-        // В рамках простого примера, перетаскивание будет работать если добавить миксин или вызвать вручную
-        
         WorldRenderEvents.AFTER_ENTITIES.register(this::renderESP);
         LOGGER.info("[freezdlc v9.0] Done!");
     }
@@ -89,17 +83,19 @@ public class BaseFinderClient implements ClientModInitializer {
 
         for (BlockPos pos : scanner.getSelectedBlocks()) {
             Box box = new Box(pos).expand(0.002);
-            double x1 = box.minX - camPos.x;
-            double y1 = box.minY - camPos.y;
-            double z1 = box.minZ - camPos.z;
-            double x2 = box.maxX - camPos.x;
-            double y2 = box.maxY - camPos.y;
-            double z2 = box.maxZ - camPos.z;
+            // Исправлено: явное приведение к float
+            float x1 = (float)(box.minX - camPos.x);
+            float y1 = (float)(box.minY - camPos.y);
+            float z1 = (float)(box.minZ - camPos.z);
+            float x2 = (float)(box.maxX - camPos.x);
+            float y2 = (float)(box.maxY - camPos.y);
+            float z2 = (float)(box.maxZ - camPos.z);
+            
             drawBox(buffer, x1, y1, z1, x2, y2, z2, r, g, b, a);
         }
     }
 
-    private void drawBox(VertexConsumer buffer, double x1, double y1, double z1, double x2, double y2, double z2, float r, float g, float b, float a) {
+    private void drawBox(VertexConsumer buffer, float x1, float y1, float z1, float x2, float y2, float z2, float r, float g, float b, float a) {
         buffer.vertex(x1, y1, z1).color(r, g, b, a).next();
         buffer.vertex(x2, y1, z1).color(r, g, b, a).next();
         buffer.vertex(x2, y1, z1).color(r, g, b, a).next();
