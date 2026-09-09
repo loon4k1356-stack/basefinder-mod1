b7eb94542988d215111a83090768938502863d64
loon4k1356-stack <loon4k1356@gmail.com>
2026-09-09 22:25:27 +0300

    Update Setting.java

--- src/main/java/com/basefinder/module/settings/Setting.java
+++ src/main/java/com/basefinder/module/settings/Setting.java
@@ -1,23 +1,9 @@
-package com.basefinder.module.settings;
+package com.basefinder.module.settings; // Или твой пакет
+import com.basefinder.module.Setting;
 
-public abstract class Setting<T> {
-    protected String name;
-    protected String description;
-    protected T value;
-    protected T defaultValue;
-    
-    public Setting(String name, String description, T defaultValue) {
-        this.name = name;
-        this.description = description;
-        this.value = defaultValue;
-        this.defaultValue = defaultValue;
+public class BoolSetting extends Setting<Boolean> {
+    public BoolSetting(String name, String desc, boolean value) {
+        super(name, desc, value);
     }
-    
-    public String getName() { return name; }
-    public String getDescription() { return description; }
-    public T getValue() { return value; }
-    public T getDefaultValue() { return defaultValue; }
-    public void setValue(T value) { this.value = value; }
-    public void reset() { this.value = defaultValue; }
-    public abstract String getValueAsString();
+    // Метод getValue() уже есть в родителе, если он правильно написан
 }
